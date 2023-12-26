b54c681ec26513fd52acf5da3a4f397c6049f9c4
parampatel07 <iamparam102@gmail.com>
2023-12-26 09:01:36 +0530

    function

---
+++ function1.c
@@ -0,0 +1,24 @@
+// Example of Function
+#include <stdio.h>
+
+void printLine()
+{
+     printf("\n-------------------------------------------\n");
+}
+
+void printStar()
+{
+     printf("\n*******************************************\n");
+}
+
+void main()
+{
+     printLine();
+     printf("Param Patel ");
+     printStar();
+     printf("Hello World ");
+     printLine();
+     printStar();
+     printf("I am Param patel");
+     printLine();
+}
---
+++ function2.c
@@ -0,0 +1,29 @@
+// Example of function
+#include <stdio.h>
+
+void printLine(char symbol,int number)
+{
+     int count = 0;
+     printf("\n");
+     for (count = 0; count < number; count++)
+     {
+          printf("%c", symbol);
+     }
+     printf("\n");
+}
+
+void main()
+{
+
+     char sign = ' ';
+     int number = 0;
+     printf("Enter symbol to print ");
+     scanf("%c",&sign);
+     printf("Enter value of number ");
+     scanf("%d",&number);
+
+
+     printf("Param patel");
+     printLine(sign,number);
+     printf("Hello world ");
+}
